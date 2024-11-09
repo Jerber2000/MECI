81eb46c842745ea8e1d78979a4036b73be547104
Jerber <jcintor@miumg.edu.gt>
2024-11-08 21:32:49 -0600

    actualizacion de detection y agregar imagenes

--- detection.py
+++ detection.py
@@ -11,62 +11,78 @@ import threading
 from kivy.graphics import Color, Rectangle
 from kivy.graphics.texture import Texture
 
+
 class IconButton(Button):
-    def __init__(self, icon_source, **kwargs):
+    def __init__(self, text, icon_source, **kwargs):
         super().__init__(**kwargs)
+
+        # Crear ícono e imagen dentro del botón
+        self.icon = Image(source=icon_source, size_hint=(None, None), size=(40, 40))  # Tamaño de la imagen
+        self.text = text
+
         self.orientation = 'horizontal'
-        # Añadir el icono fuera del botón
-        self.icon = Image(source=icon_source, size_hint=(None, None), size=(30, 30))
-        self.text_label = Label(text=self.text, color=(1, 1, 1, 1), size_hint=(None, None))
-        self.clear_widgets()
+
+        # Añadir icono y texto
         self.add_widget(self.icon)
-        self.add_widget(self.text_label)
+        self.add_widget(Label(text=self.text, color=(1, 1, 1, 1)))
+
 
 class MainApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        # Cargar el modelo YOLO una sola vez
         self.model = YOLO('last.pt')  # Asegúrate de que este archivo esté en el directorio actual
 
     def build(self):
-        # Determinar si el dispositivo es móvil o de escritorio
-        is_mobile = Window.width < 600
-
         # Crear layout principal
         self.main_layout = BoxLayout(orientation='horizontal')
 
-        # Crear el contenedor de botones y configurarlo como sidebar
+        # Crear el sidebar
         self.sidebar_expanded = True
-        self.sidebar_width = 200 if self.sidebar_expanded else 50
+        self.sidebar_width = 200
         self.button_container = BoxLayout(size_hint=(None, 1), width=self.sidebar_width, orientation='vertical',
                                           spacing=10)
 
-        # Configurar color de fondo blanco para el sidebar
+        # Fondo del sidebar
         with self.button_container.canvas.before:
-            Color(1, 1, 1, 1)  # Color blanco
+            Color(0.6, 0.8, 0.6, 1)
             self.bg_rect = Rectangle(size=self.button_container.size, pos=self.button_container.pos)
 
         self.button_container.bind(size=self.update_rect, pos=self.update_rect)
 
-        # Botón para expandir o retraer el sidebar
-        self.toggle_button = Button(text='<<' if self.sidebar_expanded else '>>', size_hint=(1, None), height=50,
-                                    background_color=(0, 1, 0, 1), color=(1, 1, 1, 1))  # Verde con texto blanco
+        # Altura uniforme para todos los botones
+        button_height = 50
+
+        # Botón de expandir/retraer sidebar
+        self.toggle_button = Button(text='<<', size_hint=(1, None), height=button_height,
+                                    background_color=(0, 1, 0, 1), color=(1, 1, 1, 1))
         self.toggle_button.bind(on_press=self.toggle_sidebar)
         self.button_container.add_widget(self.toggle_button)
 
-        # Botón "detección en tiempo real" con icono
-        real_time_button = IconButton(text='Detección en tiempo real', icon_source='camara.jpg',
+        # Imagen de cámara abajo del botón
+        camera_image = Image(source='images/camara.png', size_hint=(None, None), height=30,
+                             size=(200, 50))
+        self.button_container.add_widget(camera_image)
+
+        # Botón de cámara en tiempo real
+        real_time_button = IconButton(text='Cámara', icon_source='images/camara.png',
                                       background_color=(0, 1, 0, 1), color=(1, 1, 1, 1))
+        real_time_button.height = button_height
         real_time_button.bind(on_press=self.run_detector)
         self.button_container.add_widget(real_time_button)
 
-        # Botón "detección en foto" con icono
-        photo_button = IconButton(text='Detección en foto', icon_source='img.jpg',
+        # Imagen de carga de imagen
+        img_image = Image(source='images/img.png', size_hint=(None, None), height=30,
+                          size=(200, 50))
+        self.button_container.add_widget(img_image)
+
+        # Botón de carga de imagen
+        photo_button = IconButton(text='Cargar imagen', icon_source='images/img.png',
                                   background_color=(0, 1, 0, 1), color=(1, 1, 1, 1))
+        photo_button.height = button_height
         self.button_container.add_widget(photo_button)
 
         # Botón para cerrar la cámara
-        close_camera_button = Button(text='Cerrar Cámara', size_hint=(1, None), height=50,
+        close_camera_button = Button(text='Cerrar Cámara', size_hint=(1, None), height=button_height,
                                      background_color=(1, 0, 0, 1), color=(1, 1, 1, 1))
         close_camera_button.bind(on_press=self.close_camera)
         self.button_container.add_widget(close_camera_button)
@@ -81,41 +97,32 @@ class MainApp(App):
         return self.main_layout
 
     def update_rect(self, *args):
-        """Actualizar el tamaño y la posición del fondo blanco en el sidebar."""
         self.bg_rect.size = self.button_container.size
         self.bg_rect.pos = self.button_container.pos
 
     def toggle_sidebar(self, instance):
-        """Alterna entre los modos expandido y retraído del sidebar."""
         self.sidebar_expanded = not self.sidebar_expanded
         self.sidebar_width = 200 if self.sidebar_expanded else 50
         self.button_container.width = self.sidebar_width
         self.toggle_button.text = '<<' if self.sidebar_expanded else '>>'
 
     def run_detector(self, instance):
-        """Iniciar la detección en tiempo real."""
-        # Si la cámara ya está en funcionamiento, no hacemos nada
         if hasattr(self, 'cap') and self.cap is not None and self.cap.isOpened():
             return
 
-        # Cambiar a detección en tiempo real y ejecutar en un hilo separado
         detector_thread = threading.Thread(target=self.start_detector)
         detector_thread.daemon = True
         detector_thread.start()
 
     def start_detector(self):
-        """Iniciar la cámara."""
         self.cap = cv2.VideoCapture(0)
         if not self.cap.isOpened():
             print("Error: No se puede abrir la cámara.")
             return
 
-        # Iniciar el bucle de actualización en el hilo de la interfaz
         Clock.schedule_interval(self.update_frame, 1.0 / 30.0)
 
     def update_frame(self, dt):
-        """Actualizar el frame con detección en tiempo real."""
-        # Verificar si la cámara está disponible
         if not hasattr(self, 'cap') or self.cap is None:
             return
 
@@ -123,7 +130,6 @@ class MainApp(App):
         if not ret:
             return
 
-        # Realizar la predicción
         results = self.model.predict(source=frame, save=False, conf=0.25)
         class_names = self.model.model.names
         colors = [(0, 255, 0), (255, 0, 0)]
@@ -139,30 +145,25 @@ class MainApp(App):
                 text = f'{class_name}: {conf:.2f}'
                 cv2.putText(frame, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
 
-        # Convertir el frame a una textura y mostrarla en la imagen de Kivy
         buf = cv2.flip(frame, 0).tobytes()
         texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
         texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
         self.background.texture = texture
 
     def close_camera(self, instance):
-        """Cerrar la cámara y restaurar el estado inicial de la app."""
-        if hasattr(self, 'cap') and self.cap is not None:
-            self.cap.release()  # Liberar la cámara
-            self.cap = None
-
-        # Anular el registro de update_frame del reloj de Kivy
-        Clock.unschedule(self.update_frame)
-
-        # Restaurar la imagen de fondo original en la interfaz de Kivy
-        self.background.texture = None  # Limpiar la textura de detección
-        self.background.source = 'images/background.jpg'
-        self.background.reload()  # Recargar la imagen de fondo
-
-    def on_stop(self):
-        # Liberar la cámara al cerrar la app
         if hasattr(self, 'cap') and self.cap is not None:
             self.cap.release()
+            self.cap = None
+
+        Clock.unschedule(self.update_frame)
+        self.background.texture = None
+        self.background.source = 'images/background.jpg'
+        self.background.reload()
+
+    def on_stop(self):
+        if hasattr(self, 'cap') and self.cap is not None:
+            self.cap.release()
+
 
 # Ejecutar la aplicación
 if __name__ == '__main__':
